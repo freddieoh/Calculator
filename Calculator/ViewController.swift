773baaf931092ac154632f4c4564280127747112
Fredrick Ohen <fredrickohen@gmail.com>
2017-10-24 13:28:58 -0500

    Added computed properties for operations

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,18 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+  var userIsInTheMiddleOfTyping = false
+  
   override func viewDidLoad() {
     super.viewDidLoad() 
   }
+  
+  
 
+ 
   @IBOutlet weak var digitDisplayLabel: UILabel!
   
-  @IBAction func touchDigit(_ sender: UIButton) {
-    
-    let digit = sender.currentTitle
-    print("\(String(describing: digit)) was touched")
+  var displayValue: Double
+  {
+    get {
+      return Double(digitDisplayLabel.text)!
+    }
+    set {
+      digitDisplayLabel.text = String(newValue)
+    }
   }
   
-
+  @IBAction func performOperation(_ sender: UIButton) {
+  
+    userIsInTheMiddleOfTyping = false
+    
+    if let mathmaticalSymbol = sender.currentTitle {
+      switch mathmaticalSymbol {
+      case "π":
+        displayValue = Double.pi
+      case "√":
+        displayValue = sqrt(displayValue)
+      default:
+        break
+      }
+    }
+  }
+  
+  @IBAction func touchDigit(_ sender: UIButton) {
+    let digit = sender.currentTitle!
+    
+    if userIsInTheMiddleOfTyping {
+      let textCurrentlyInDisplay = digitDisplayLabel.text!
+      digitDisplayLabel.text = textCurrentlyInDisplay + digit
+    } else {
+      digitDisplayLabel.text = digit
+      userIsInTheMiddleOfTyping = true
+    }
+  }
 }
 
